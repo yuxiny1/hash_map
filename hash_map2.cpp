36592020c0be956bd763a6bededd6dca4f7cd047
E <62050298+yuxiny1@users.noreply.github.com>
2023-07-07 19:33:39 +0100

    update
    update

---
+++ hash_map2.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+class HashTable{
+    private:
+    static const int hashGroups = 20;
+    vector<unordered_map<int, std::string> > table;
+
+    public: 
+        HashTable() : table(hashGroups) {}
+
+        int hashFunction(int key);
+        void insertItem(int key, string value);
+        void removeItem(int key);
+        string searchTable(int key);
+        void printTable();
+    };
+
+
+    int HashTable::hashFunction(int key){
+        return key % hashGroups; 
+    }
+
+    void HashTable::insertItem(int key, string value){
+        int hashValue = hashFunction(key);
+        auto& cell = table[hashValue];
+
+        auto itr = cell.find(key);
+        if(itr != cell.end()){
+            cout<<"[WARNING] key exit; value replaced."<<endl;
+            itr->second = value;
+        }else{
+            cell.insert({key, value});
+        }
+    }
+
+    void HashTable::removeItem(int key){
+        int hashValue = hashFunction(key);
+        auto& cell = table[hashValue];
+        auto itr = cell.find(key);
+        if(itr != cell.end()){
+            cout << "[INFO] key exits. value removed" << endl;
+            cell.erase(itr);
+        }else{
+            cout << "[WARNING] item not found " << endl;
+        }
+    }
+
+    string HashTable::searchTable(int key){
+        int hashValue = hashFunction(key);
+        auto& cell = table[hashValue];
+        auto itr = cell.find(key);
+        if(itr != cell.end()){
+            return itr->second;
+        }else{
+            cout << "[WARNING] Key not found.\n";
+            return {};
+        }
+    }
+
+    void HashTable::printTable(){
+        for(int i = 0; i < hashGroups; ++i){
+            if(table[i].empty()) continue;
+            for(const auto& kv : table[i]){
+                cout << "[INFO] key:" << kv.first << " value: " << kv.second << endl;
+            }
+        }
+    }
+
+int main(){
+    HashTable HT;
+
+    HT.insertItem(905,"jim");
+    HT.insertItem(345,"sam");
+    HT.insertItem(234,"alex");
+    HT.insertItem(231,"sandy");
+    HT.insertItem(123,"rob");
+
+    HT.printTable();
+
+    HT.removeItem(322);
+    HT.removeItem(123);
+
+    return 0;
+}
--- hashmap.cpp
+++ hashmap.cpp
@@ -3,118 +3,157 @@
 #include <cstring>
 using namespace std;
 
-//hashtable to implement 905, jimmy 
-
-class HashTable{
-    private:
-    static const int hashGroups = 10;
+class HashTable
+{
+private:
+    static const int hashGroups = 20;
     list<pair<int, std::string > > table[hashGroups];
 
-    public: 
+public:
+    bool isEmpty() const;
+    int hashFunction(int key);
+    void insertItem(int key, string value);
+    void removeItem(int key);
+    string searchTable(int key);
+    void printTable();
+};
 
-        bool isEmpty() const;
-        int hashFunction(int key);
-        void insertItem(int key, string value);
-        void removeItem(int key);
-        string searchTable(int key);
-        void printTable();
-    };
-
-
-    bool HashTable::isEmpty() const{
-        int sum{};
-        for(int i{}; i<hashGroups; i++){
-            sum += table[i].size();
-        }
-        if(!sum){
-            return true;
-        }
-        return false;
-    };
-
-
-    int HashTable::hashFunction(int key){
-        return key% hashGroups; //key : 905, in return, this functio nwill sp[lit out 5]
+bool HashTable::isEmpty() const
+{
+    int sum{};
+    for (int i{}; i < hashGroups; i++)
+    {
+        sum += table[i].size();
     }
-
-    void HashTable::insertItem(int key, string value){
-        int hashValue = hashFunction(key);
-        auto& cell = table[hashValue];
-        auto bItr = begin(cell);
-        bool keyExists=false;
-        for(; bItr !=end(cell); bItr++){
-            if (bItr->first==key){
-                keyExists=true;
-                bItr->second=value;
-                cout<<"[WARNING] key exit; value replaced."<<endl; 
-                break;
-            }
-        }
-
-        if(!keyExists){
-            cell.emplace_back(key,value);
-        }
-            return;
+    if (!sum)
+    {
+        return true;
     }
+    return false;
+};
 
+int HashTable::hashFunction(int key)
+{
+    return key % hashGroups; // key : 905, in return, this functio nwill sp[lit out 5]
+}
 
-    void HashTable::removeItem(int key){
-        int hashValue= hashFunction(key);
-        auto&cell = table[hashValue];
-        auto bItr =begin(cell);
-        bool keyExists=false;
-        for(; bItr!=end(cell); bItr++){
-            if(bItr->first==key){
-            keyExists= true;
-            bItr=cell.erase(bItr); // it will return next iterator ,no need to continue 
-            cout<<"[INFO] key exits. value removed" <<endl;
+void HashTable::insertItem(int key, string value)
+{
+    int hashValue = hashFunction(key);
+    auto &cell = table[hashValue];
+    auto bItr = begin(cell);
+    bool keyExists = false;
+    for (; bItr != end(cell); bItr++)
+    {
+        if (bItr->first == key)
+        {
+            keyExists = true;
+            bItr->second = value;
+            cout << "[WARNING] key exit; value replaced." << endl;
             break;
         }
-        }
-        if(!keyExists){
-            cout<<"[WARNING] iterm not found "<<endl;
-        }
     }
 
-    void HashTable::printTable(){
-        for(int i{}; i<hashGroups; ++i){
-            if(table[i].size()==0) continue;
+    if (!keyExists)
+    {
+        cell.emplace_back(key, value);
+    }
+    return;
+}
 
-            auto bItr= table[i].begin();
-            for(; bItr != table[i].end(); bItr++){
-                cout<<"[INFO] key:" <<bItr->first<<"value: "<<bItr->second<<endl;
-            }
-
+void HashTable::removeItem(int key)
+{
+    int hashValue = hashFunction(key);
+    auto &cell = table[hashValue];
+    auto bItr = begin(cell);
+    bool keyExists = false;
+    for (; bItr != end(cell); bItr++)
+    {
+        if (bItr->first == key)
+        {
+            keyExists = true;
+            bItr = cell.erase(bItr); // it will return next iterator ,no need to continue
+            cout << "[INFO] key exits. value removed" << endl;
+            break;
         }
-        return;
     }
-
-    int main(){
-        HashTable HT;
-
-        if(HT.isEmpty()){
-            cout<<"correct answer . good job "<<endl;
-        }else{
-            cout<<"ohoh, we need to check out code"<<endl;
-        };
-
-        HT.insertItem(905,"jim");
-        HT.insertItem(345,"sam");
-        HT.insertItem(234,"alex");
-        HT.insertItem(231,"sandy");
-        HT.insertItem(123,"rob");
-
-        HT.printTable();
-
-        HT.removeItem(322);
-        HT.removeItem(123);
-
-
-        if(HT.isEmpty()){
-            cout<<"some mistakes"<<endl;
-        }else{
-            cout<<"ok no problem so far "<<endl;
-        };
-
-        return 0; 
+    if (!keyExists)
+    {
+        cout << "[WARNING] iterm not found " << endl;
     }
+}
+
+void HashTable::printTable()
+{
+    for (int i{}; i < hashGroups; ++i)
+    {
+        if (table[i].size() == 0)
+            continue;
+
+        auto bItr = table[i].begin();
+        for (; bItr != table[i].end(); bItr++)
+        {
+            cout << "[INFO] key:" << bItr->first << "value: " << bItr->second << endl;
+        }
+    }
+    return;
+}
+
+string HashTable::searchTable(int key)
+{
+    int hashValue = hashFunction(key);
+    auto &cell = table[hashValue];
+    // iterator std::list<pair<int, std::string>>::iterator
+    auto bItr = begin(cell);
+    bool keyExists = false;
+    for (; bItr != end(cell); bItr++)
+    {
+        if (bItr->first == key)
+        {
+            keyExists = true;
+            return bItr->second;
+        }
+    }
+    if (!keyExists)
+    {
+        cout << "[WARNING] Key not found.\n";
+        return {};
+    }
+    return {};
+}
+
+int main()
+{
+    
+    HashTable HT;
+
+    if (HT.isEmpty())
+    {
+        cout << "correct answer . good job " << endl;
+    }
+    else
+    {
+        cout << "ohoh, we need to check out code" << endl;
+    };
+
+    HT.insertItem(905, "jim");
+    HT.insertItem(345, "sam");
+    HT.insertItem(234, "alex");
+    HT.insertItem(231, "sandy");
+    HT.insertItem(123, "rob");
+
+    HT.printTable();
+
+    HT.removeItem(322);
+    HT.removeItem(123);
+
+    if (HT.isEmpty())
+    {
+        cout << "some mistakes" << endl;
+    }
+    else
+    {
+        cout << "ok no problem so far" << endl;
+    };
+    return 0;
+};
